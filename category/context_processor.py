751736598b46b7b84aef15b54ba43e21c6447fd3
esejosue <josuegallardolara@hotmail.com>
2022-11-29 15:19:29 -0800

    11/29/2022 Added checkout

--- category/context_processor.py
+++ category/context_processor.py
@@ -1,4 +1,6 @@
 from .models import Category
+from os import link
+from sre_constants import CATEGORY
 
 #function that will create a link to all of the categories saved in datas
 def categoryMenu(request):
