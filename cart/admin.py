30e281c425d9650ed2da87f3392b8d5141e01825
Christian Contreras <91349379+ChristianC343@users.noreply.github.com>
2022-12-01 22:40:39 -0800

    Delete cart directory

--- cart/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CartItem
-
-class CartAdmin(admin.ModelAdmin):
-    list_display = ("product", "quantity", "is_active")
-
-admin.site.register(CartItem, CartAdmin)
