118187aafd0de5d40d65b7ed22c9e880b95640b7
Christian Contreras <91349379+ChristianC343@users.noreply.github.com>
2022-12-01 22:41:00 -0800

    Delete inventory directory

--- inventory/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product
-
-# Register your models here.
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("product_name", "price", "stock", "modified_date", "is_available")
-    
-
-admin.site.register(Product, ProductAdmin)
