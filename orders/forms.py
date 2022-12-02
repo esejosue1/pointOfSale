467cd38381b0e31fd2a459f570df249209dcb720
Christian Contreras <91349379+ChristianC343@users.noreply.github.com>
2022-12-02 10:58:05 -0800

    Delete orders directory

--- orders/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Order
-class orderForm(forms.ModelForm):
-    class meta:
-        models = Order
-        fields = ["first_name", "last_name", "phone","email",
-        "address", "address2", "state","city","zip"
-         ]
