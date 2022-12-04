e427dd1a6d965e400ca6be496fb8ebc9b7a9ed7b
esejosue <josuegallardolara@hotmail.com>
2022-12-03 20:42:40 -0800

    Order model can now save into admin, still needs to fix the total and tax for Orders/models

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from .models import Order
-class orderForm(forms.ModelForm):
-    class meta:
-        models = Order
-        fields = ["first_name", "last_name", "phone","email",
-        "address", "address2", "state","city","zip"
-         ]
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ["first_name", "last_name", "phone","email","address", "address2", "state","city","zip"]
--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
-from .forms import orderForm
+from django.shortcuts import render,redirect
+from .forms import OrderForm
 from .models import Order
+import datetime
 # Create your views here.
 
 
 def completeOrder(request):
     if request.method == "POST": 
-        form = orderForm(request.POST)
+        form = OrderForm(request.POST)
         if form.is_valid():
             data = Order()
             data.first_name = form.cleaned_data["first_name"]
@@ -18,12 +19,25 @@ def completeOrder(request):
             data.state = form.cleaned_data["state"]
             data.city = form.cleaned_data["city"]
             data.zip = form.cleaned_data["zip"]
-        
-            data.save()
-        return render(request, "checkout.html")
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.order_total = 0
+            data.tax = 0
 
-    else:
-        return render(request, "index.html")
+            data.save()
+            # order number
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr, mt, dt)
+            current_date = d.strftime("%Y%m%d")
+            o_number = current_date + str(data.id)
+            data.order_number = o_number
+            data.save()
+            return render(request, "checkout.html")
+        else:
+            print(form.errors)
+       
+
  
 def orders(request):
     return render(request, "checkout.html")
